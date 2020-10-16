d5d4b6881ff92bebb748e5571494e9f086e877a0
Maeesha <maeesha.rahman@gmail.com>
2020-10-16 13:27:07 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+
+
+// if there is an environment variabe that has already been defined by heroku assign that variable to PORT otherwise use value of 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+// http://localhost:5000/
