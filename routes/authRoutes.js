c81b635e6c26b964ac6aa18c910647e29ff1462f
Maeesha <maeesha.rahman@gmail.com>
2020-10-20 16:04:48 -0400

    set up auth action and reducer

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,14 +8,21 @@ module.exports = (app) => {
         })
     )
 
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get(
+        '/auth/google/callback',
+        passport.authenticate('google'),
+        (req, res) => {
+            res.redirect('/surveys');
+        }
+    );
 
     // user logout
     app.get('/api/logout', (req, res) => {
         // logout is a function that is automatically attached to the req object by passport 
         req.logout();
         // req that user is destroyed by passport since we are no longer signed in 
-        res.send(req.user);
+        // res.send(req.user);
+        res.redirect('/');
     })
 
     app.get('/api/current_user', (req, res) => {
--- services/passport.js
+++ services/passport.js
@@ -26,19 +26,21 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id })
-                .then((existingUser) => {
-                    if (existingUser) {
-                        // we already have a record with the given profile id 
-                        done(null, existingUser);
-                    } else {
-                        // we don't have a user record with this id, make a new record
-                        // take model instance and save to mongo database 
-                        new User({ googleId: profile.id }).save()
-                            .then(user => done(null, user));
-                    }
-                })
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id })
+
+            if (existingUser) {
+                // we already have a record with the given profile id 
+                return done(null, existingUser);
+            }
+            // we don't have a user record with this id, make a new record
+            // take model instance and save to mongo database 
+            const user = await new User({ googleId: profile.id }).save();
+            done(null, user);
+
         }
     )
 );
+
+
+
