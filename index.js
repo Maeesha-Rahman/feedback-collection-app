38a6b867f807eaa74aaa580f6a1ff38b5a532cb5
Maeesha <maeesha.rahman@gmail.com>
2020-10-22 14:06:42 -0400

    package.json config

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // express will serve up production assets like our main.js file, or main.css file 
+    // if any get request comes in for some route or file, and we don't understand what it is looking for (if we don't have a route handler set up) then look into clien/build directory and try to see if there's some file inside that matches up with what the request is looking for
+    app.use(express.static('client/build'));
+
+    // exporess will serve up the index.html file if it doesn't recognize the route 
+    // if there's also no file inside of client/build directory that matches up with what the request is looking for, give them back index.html file (catch all case)
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 
 // if there is an environment variabe that has already been defined by heroku assign that variable to PORT otherwise use value of 5000
 const PORT = process.env.PORT || 5000;
